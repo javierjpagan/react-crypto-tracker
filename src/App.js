64c0fa77b8e5f11657cd365c7509f1382ab61462
Javier Pagan <9905163+javierjpagan@users.noreply.github.com>
2021-12-17 15:38:55 -0500

    Added Search bar

--- src/App.js
+++ src/App.js
@@ -19,9 +19,15 @@ axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=
   return (
     <div className="coin-app">
       <div className='coin-search'>
-<h1 className='coin-text'>API</h1>
+<h1 className='coin-text'>Search a currency</h1>
+<form>
+<input type="text" placeholder="Search"
+className='coin-input'/>
+</form>
       </div>
+      
     </div>
+
   );
 }
 
